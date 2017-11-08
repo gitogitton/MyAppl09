2c3f3946797fe50ec558a3ab6a43e5dde4903d75
gitogitton <mukyu_02@yahoo.co.jp>
2017-11-08 15:18:42 +0900

    ファイルリストの初期表示されたので、この時点で Warning を取り除いてみた。

--- app/src/main/java/com/example/user/myappl09/CustomAdapter.java
+++ app/src/main/java/com/example/user/myappl09/CustomAdapter.java
@@ -1,9 +1,7 @@
 package com.example.user.myappl09;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -12,23 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Handler;
-
-import static android.util.TypedValue.COMPLEX_UNIT_SP;
 
 // package-private class
 class CustomAdapter extends ArrayAdapter<File> {
 
     private final String CLASS_NAME = "CustomAdapter";
-    LayoutInflater mInflater;
+    private LayoutInflater mInflater;
 
-    CustomAdapter(Context context,List objects) {
+    CustomAdapter(Context context,List<File> objects) {
         super(context, 0, objects);
         mInflater = LayoutInflater.from(context);
         Log.d( CLASS_NAME, "constructor run." );
@@ -46,21 +39,23 @@ class CustomAdapter extends ArrayAdapter<File> {
         Log.d(CLASS_NAME,"getView() start. [convertView:"+convertView.getId()+"]");
 
         //表示するデータを取得
-        File info = getItem(position);
+        File item = getItem(position);
         TextView textView = (TextView)convertView.findViewById(R.id.textView);
         if(textView==null) { Log.d(CLASS_NAME,"tv is null."); }   //debug
-        String msg = (info!=null?info.getName():"null");    //debug
-        Log.d(CLASS_NAME,"info.getName()="+msg);    //debug
-        textView.setText(info.getName());  //ファイル名
-        Drawable icon;  //アイコン
-        if (info.isDirectory()) {
-            icon = ContextCompat.getDrawable(getContext(), R.drawable.ic_folder_black_24dp);
-        }else{
-            icon = ContextCompat.getDrawable(getContext(), R.drawable.ic_file);
-        }
-        icon.setBounds( 0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight() ); //ICONの表示位置を設定 (引数：座標 x, 座標 y, 幅, 高さ)
-        textView.setCompoundDrawables( icon, null, null, null ); //TextViewにアイコンセット（四辺(left, top, right, bottom)に対して別個にアイコンを描画できる）
-
+        String msg = (item!=null?item.getName():"null");    //debug
+        Log.d(CLASS_NAME,"item.getName()="+msg);    //debug
+//        assert textView != null;
+        if (textView!=null) {
+            textView.setText(item != null ? item.getName() : "null");  //ファイル名
+            Drawable icon;  //アイコン
+            if (item != null && item.isDirectory()) {
+                icon = ContextCompat.getDrawable(getContext(), R.drawable.ic_folder_black_24dp);
+            } else {
+                icon = ContextCompat.getDrawable(getContext(), R.drawable.ic_file);
+            }
+            icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight()); //ICONの表示位置を設定 (引数：座標 x, 座標 y, 幅, 高さ)
+            textView.setCompoundDrawables(icon, null, null, null); //TextViewにアイコンセット（四辺(left, top, right, bottom)に対して別個にアイコンを描画できる）
+        }//if(textView!=null)
         Log.d(CLASS_NAME,"getView() fin.");
 
         return convertView;
