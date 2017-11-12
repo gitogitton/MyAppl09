70272d666b5a58b191ebb49e33b532404cb96ccb
gitogitton <mukyu_02@yahoo.co.jp>
2017-11-12 16:06:36 +0900

    ・コピー処理完了。 既出の不具合（行が増える、選択が０）の件は解決。

--- app/src/main/java/com/example/user/myappl09/MainActivity.java
+++ app/src/main/java/com/example/user/myappl09/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -83,6 +84,70 @@ public class MainActivity extends AppCompatActivity {
     } //onCreate()
 
     // ============================================================================================================
+    //overflowmenを表示
+    //  どこから呼ばれるかは自分で確認してみよう！！！親クラスから呼ばれてるんだろうけれど。
+    //  menu_main.xmlを定義し、本メソッドのオーバーライドでツールバーの端に縦点３つのやつが出てきた。
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        getMenuInflater().inflate( R.menu.menu_main, menu );
+        return true;
+    }
+
+    // ============================================================================================================
+    //overflowmenuのイベントを処理
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        Log.d( APPL_MAME, "onOptionsItemSelected() --- start. [ menu = " + item.toString() + " ]" );
+        switch( item.getItemId() ) {
+            case R.id.action_upFolder :
+                String path = getParentDirectoryName();
+                if(path.length()>0) {
+                    showListOfDirectory(path);
+                }
+                break;
+            case R.id.action_appEnd :
+                showAlertDialog(R.string.alertDlg_message_fin);
+                break;
+            case R.id.action_upRoot :
+                showListOfDirectory(FILE_SEPARATOR);
+                break;
+            case R.id.action_copy :
+                //コピー対象のファイルの情報を保存（実際のコピー処理はペーストされたタイミングで行う）
+                copyCheckedFileInfo();
+                break;
+            default:
+                //do nothing
+                break;
+        }
+        return super.onOptionsItemSelected( item );
+    }
+
+    // ============================================================================================================
+    //ナビゲーションボタンのイベントを受ける。
+    @Override
+    public boolean onKeyDown( int keyCode, KeyEvent event ) {
+
+        Log.d( APPL_MAME, "onKeyDown() ---start." );
+
+        if ( KeyEvent.KEYCODE_BACK==keyCode ) {
+            //現在表示中のパス文字列を取得
+            TextView textView = (TextView)findViewById(R.id.textView4);
+            String path = textView.getText().toString();
+            if (path.equals(FILE_SEPARATOR)) {
+                //ダイアログで終了を確認する。
+                showAlertDialog(R.string.alertDlg_message_fin);
+            } else {
+                //直上のディレクトリーのパス取得
+                String parentPath = getParentDirectoryName();
+                showListOfDirectory(parentPath);
+            }
+            return true;
+        }
+        Log.d( APPL_MAME, "onKeyDown() fin." );
+        return super.onKeyDown( keyCode, event );
+    }
+
+    // ============================================================================================================
     //指定されたディレクトリの内容をリストに設定
     private void showListOfDirectory(String directoryName) {
         Log.d(APPL_MAME,"showListOfDirectory start. [args="+directoryName+"]");
@@ -113,19 +178,20 @@ public class MainActivity extends AppCompatActivity {
         item.toggle();
         //リストにセット
         CustomAdapter adapter = (CustomAdapter) listView.getAdapter();
-        adapter.add(item);
-//call automatically when add(),remove(),clear() are called.        adapter.notifyDataSetChanged();
+        adapter.notifyDataSetChanged();
         Log.d(APPL_MAME,"selectItem() fin.");
     }
     //---------------------------------------------------------------------------
     //checkされている項目を取得
     private ArrayList<LineData> getCheckedItems() {
         ArrayList<LineData> items = new ArrayList<>();
-        for (LineData everyItem : items) {
-            if (!everyItem.isChecked()) {
-                continue;
+        ListView listView = (ListView)findViewById(R.id.fileList01);
+        ListAdapter listAdapter = listView.getAdapter();
+        for ( int i=0; i<listAdapter.getCount(); i++ ) {
+            LineData item = (LineData)listAdapter.getItem(i);
+            if (item.isChecked()) {
+                items.add(item);
             }
-            items.add(everyItem);
         }
         Log.d(APPL_MAME,"getCheckedItems() size="+items.size());
         return items;
@@ -164,43 +230,6 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(arrayAdapter);    //listviewにadapterを指定
 
         Log.d( APPL_MAME, "setList() --- end.");
-    }
-    // ============================================================================================================
-    //overflowmenを表示
-    //  どこから呼ばれるかは自分で確認してみよう！！！親クラスから呼ばれてるんだろうけれど。
-    //  menu_main.xmlを定義し、本メソッドのオーバーライドでツールバーの端に縦点３つのやつが出てきた。
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu ) {
-        getMenuInflater().inflate( R.menu.menu_main, menu );
-        return true;
-    }
-    // ============================================================================================================
-    //overflowmenuのイベントを処理
-    @Override
-    public boolean onOptionsItemSelected( MenuItem item ) {
-        Log.d( APPL_MAME, "onOptionsItemSelected() --- start. [ menu = " + item.toString() + " ]" );
-        switch( item.getItemId() ) {
-            case R.id.action_upFolder :
-                String path = getParentDirectoryName();
-                if(path.length()>0) {
-                    showListOfDirectory(path);
-                }
-                break;
-            case R.id.action_appEnd :
-                showAlertDialog(R.string.alertDlg_message_fin);
-                break;
-            case R.id.action_upRoot :
-                showListOfDirectory(FILE_SEPARATOR);
-                break;
-            case R.id.action_copy :
-                //コピー対象のファイルの情報を保存（実際のコピー処理はペーストされたタイミングで行う）
-                copyCheckedFileInfo();
-                break;
-            default:
-                //do nothing
-                break;
-        }
-        return super.onOptionsItemSelected( item );
     }
 
     //--------------------------------------------
@@ -246,30 +275,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(APPL_MAME,"getParentDirectoryName() edited path="+editedPath);
         return editedPath;
     }
-    // ============================================================================================================
-    //ナビゲーションボタンのイベントを受ける。
-    @Override
-    public boolean onKeyDown( int keyCode, KeyEvent event ) {
 
-        Log.d( APPL_MAME, "onKeyDown() ---start." );
-
-        if ( KeyEvent.KEYCODE_BACK==keyCode ) {
-            //現在表示中のパス文字列を取得
-            TextView textView = (TextView)findViewById(R.id.textView4);
-            String path = textView.getText().toString();
-            if (path.equals(FILE_SEPARATOR)) {
-                //ダイアログで終了を確認する。
-                showAlertDialog(R.string.alertDlg_message_fin);
-            } else {
-                //直上のディレクトリーのパス取得
-                String parentPath = getParentDirectoryName();
-                showListOfDirectory(parentPath);
-            }
-            return true;
-        }
-        Log.d( APPL_MAME, "onKeyDown() fin." );
-        return super.onKeyDown( keyCode, event );
-    }
 // 以下はDialogFragment のため追加
     // ============================================================================================================
     // alertDialog を表示する。
